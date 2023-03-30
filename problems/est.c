3e14fcb28f9b773f3f4ecf44bd939a3137ed8c20
Thisgrama <thiagosantos4050@gmail.com>
2023-03-29 21:00:39 -0300

    a

--- problems/est.c
+++ problems/est.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 
+int multiples(int a, int b, int k) {
+    int result = 0;
+    int i = 1;
+    for (i; i < k; i++) {
+        if (i % a == 0 || i % b == 0) {
+            result += i;
+        }
+    }
+    return result;
+}
+
 int main() {
-    int number;
-
-    printf("enter a tarara: ");
-
-    scanf("%d", number);
-
-    printf("Voce digitou %d", number);
-
+    printf("%d", multiples(3, 5, 1000));
     return 0;
 }
